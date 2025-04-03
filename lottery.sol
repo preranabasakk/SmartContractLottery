2c0d69d16e0d3b8dfc42dbf4f62c957238ecdeef
preranabasakk <basakprerna7@gmail.com>
2025-04-03 10:34:45 +0530

    Add files via upload

---
+++ lottery.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.5.0 <0.9.0;
+contract lottery{
+    address public manager ;
+    address payable [] public participants;
+
+     constructor(){
+        manager=msg.sender; //global varaiable 
+     }
+
+   //   receive() external payable{
+   //    require (msg.value==1 ether);
+   //      participants.push(payable(msg.sender));
+   //   }
+
+   receive() external payable {
+    require(msg.value >= 1 ether, "Must send at least 1 ether to join");
+    participants.push(payable(msg.sender));
+}
+
+
+     function getBalance() public view returns (uint){
+      require (msg.sender == manager);
+      return address(this).balance;
+     }
+     function random() public view returns(uint){
+       return uint(keccak256(abi.encodePacked(block.prevrandao,block.timestamp,participants.length)));
+     }
+
+     function selectWinner() public {
+      require(msg.sender==manager);
+      require(participants.length>=3);
+      uint r = random();
+      address payable winner;
+
+      uint index= r% participants.length;
+      winner=participants[index];
+      winner.transfer(getBalance());
+
+     }
+}
